9c4c031f96d207adc7fade2cd6a0da816346c854
Likeur <jacksimba28@gmail.com>
2025-01-02 23:32:19 +0200

    config tailus html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "media",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: ({ colors }) => ({
+          primary : colors.blue,
+          danger : colors.rose,
+          warning : colors.yellow,
+          success : colors.lime,
+          info : colors.blue,
+          gray : colors.zinc,
+      }),
+  }
   },
   plugins: [],
 }
