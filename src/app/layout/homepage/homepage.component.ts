6b2668d91cd5fde8bc67b569754a1be60a4b0a58
Likeur <jacksimba28@gmail.com>
2025-01-03 23:43:29 +0200

    working on the navbar

--- src/app/layout/homepage/homepage.component.ts
+++ src/app/layout/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RessourceListComponent } from '../../components/ressource-list/ressource-list.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent, RessourceListComponent],
   templateUrl: './homepage.component.html',
 })
 export default class HomepageComponent {
