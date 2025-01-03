8631944a0bc22f05fb61c7e8f130938772633136
Likeur <jacksimba28@gmail.com>
2025-01-03 23:30:09 +0200

    adding routing and layout

---
+++ src/app/components/ressource-list/ressource-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ressource-list',
+  imports: [],
+  templateUrl: './ressource-list.component.html',
+  styles: ``
+})
+export class RessourceListComponent {
+
+}
---
+++ src/app/layout/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [RouterModule],
+  templateUrl: './homepage.component.html',
+})
+export default class HomepageComponent {
+
+}
