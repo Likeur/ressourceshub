f34958204d593a560455bde2a409ac95a2fa61d5
Likeur <jacksimba28@gmail.com>
2025-01-04 23:55:31 +0200

    adding footer component

--- src/app/layout/homepage/homepage.component.ts
+++ src/app/layout/homepage/homepage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { RessourceListComponent } from '../../components/ressource-list/ressource-list.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [RouterModule, NavbarComponent, RessourceListComponent],
+  imports: [RouterModule, NavbarComponent, RessourceListComponent, FooterComponent],
   templateUrl: './homepage.component.html',
 })
 export default class HomepageComponent {
